4950e3ec5db934ff45c8f58e779459a8c02b6dba
Isaac Machakatagit <isaac@caasi.co.zw>
2023-05-01 07:48:10 +0200

    feat: add response class

---
+++ FeesManagement/Models/Response.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using ICSharpCode.Core;
+
+namespace FeesManagement.Models
+{
+	public class Response
+	{
+		public bool HasError { get; set; }
+		public string Message { get; set; }
+	}
+}
