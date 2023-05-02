a50f6e234848de3037dbeb0384dd59d4c046387f
Isaac Machakatagit <isaac@caasi.co.zw>
2023-05-02 04:49:57 +0200

    refactor: functions feat: seed classes

--- FeesManagement/Models/Classes.cs
+++ FeesManagement/Models/Classes.cs
@@ -12,13 +12,22 @@ namespace FeesManagement.Models
         public Classes()
         {
         	ClassList = new Dictionary<int,string>();
+        	Seed();
         }
-        public Classes addClass(int Grade,string Name){
+        public Classes AddClass(int Grade,string Name){
         	ClassList[Grade] = Name;
         	return this;
         }
-        public string getClass(int Grade){
+        public string GetClass(int Grade){
        		return ClassList[Grade];
+        }
+        void Seed(){
+        	AddClass(1, "Form 1");
+        	AddClass(2, "Form 2");
+        	AddClass(3, "Form 3");
+        	AddClass(4, "Form 4");
+        	AddClass(5, "Form 5");
+        	AddClass(6, "Form 6");
         }
     }
 }
